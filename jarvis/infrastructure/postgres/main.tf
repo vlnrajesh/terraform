c10bf99499d2ccf7a06d0a7addd910fd608b7573
Rajesh Jonnalagadda <vlnrajeshj@gmail.com>
2021-06-28 14:51:33 +0530

    Updated Variables in Git

--- jarvis/infrastructure/postgres/main.tf
+++ jarvis/infrastructure/postgres/main.tf
@@ -1,16 +1,15 @@
 resource "docker_container" "postgres" {
-  name = "${var.postgres_container_name}"
+  name = var.postgres_container_name
   image = "${var.postgres_image_name}:${var.postgres_image_tag}"
-  restart = "on-failure"
+  restart = "always"
   must_run = "true"
   ports {
-    internal = "${var.postgres_db_port}"
-    external = "${var.postgres_db_port}"
-
+    internal = var.postgres_db_port
+    external = var.postgres_db_port
   }
   volumes {
-    container_path = "${var.pgdata_container_path}"
-    host_path = "${var.pgdata_local_path}"
+    container_path = var.pgdata_container_path
+    host_path = var.pgdata_local_path
     read_only = false
   }
   env = [
